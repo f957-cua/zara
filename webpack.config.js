30d9e64c17f6485ce495cc01da757a8d2b336e59
f957-cua <ivanfiliptschik00@gmail.com>
2023-07-18 15:49:02 +0300

    css_modules_&_minification_plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  mode: "development",
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "index_bundle.js",
@@ -27,11 +29,16 @@ module.exports = {
         exclude: /node_modules/,
         use: "babel-loader",
       },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "public", "index.html"),
     }),
+    new MiniCssExtractPlugin(),
   ],
 };
