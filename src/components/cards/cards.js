ad34cfd09c01cf359731bdda4ee8fc5dd3eb5a5b
f957-cua <ivanfiliptschik00@gmail.com>
2023-07-19 04:01:46 +0300

    links to podcast list

--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -1,20 +1,23 @@
 import React from "react";
 import "./index.css";
 
-const Cards = ({ podcasts = [] }) => {
+import { Link } from "react-router-dom";
 
+const Cards = ({ podcasts = [] }) => {
   return (
     <ul className="cards">
       {podcasts.map((card) => (
-        <li key="{card?.id.attributes.['im:id']}" className="card">
-          <img
-            src={card?.["im:image"][2].label}
-            alt={card?.["im:name"].label}
-            className="card_img"
-          />
-          <h2>{card?.["im:name"].label}</h2>
-          <p>{card?.["im:artist"].label}</p>
-        </li>
+        <Link to="podcast" key="{card?.id.attributes.['im:id']}">
+          <li className="card">
+            <img
+              src={card?.["im:image"][2].label}
+              alt={card?.["im:name"].label}
+              className="card_img"
+            />
+            <h2>{card?.["im:name"].label}</h2>
+            <p>{card?.["im:artist"].label}</p>
+          </li>
+        </Link>
       ))}
     </ul>
   );
